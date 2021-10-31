9d75aa2cd58e07d5ee7da4b5fc307a0cb5cb3b2c
Haydz <haydnit@gmail.com>
2021-10-31 19:30:32 -0400

    Create math_exercise_1.go

---
+++ math_exercise_1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	answer_a := (2 * 59) / 4
+	fmt.Println(answer_a)
+
+	answer_b := 40 * 40 * 40
+	fmt.Println(answer_b)
+
+	fmt.Println(answer_a > answer_b)
+}
